c594e70be3a44c7d74f9318d6ef4308b381d26f6
honesty-yoo <yhm9207@naver.com>
2021-02-08 22:53:00 +0900

    add Persistence Entity

---
+++ src/main/java/study/domain/posts/Posts.java
@@ -0,0 +1,35 @@
+package study.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    // 기본키 설정
+    // Identity 설정 시 자동 increment
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 컬럼 선언
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}

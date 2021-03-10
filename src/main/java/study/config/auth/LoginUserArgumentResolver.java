fcc4a33c0771f20fb790af5b5949adaf9538a9ac
honesty-yoo <yhm9207@naver.com>
2021-03-11 03:33:56 +0900

    LoginUser 어노테이션 추가
    - LoginUser 어노테이션 추가

---
+++ src/main/java/study/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,45 @@
+package study.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import study.config.auth.dto.SessionUser;
+import study.web.LoginUser;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    /**
+     * 파라미터를 사용하는지 판별함
+     * @param 함수 파라미터
+     * @return 파라미터 사용여부
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    /**
+     * 파라미터로 전달할 값
+     * @param parameter
+     * @param mavContainer
+     * @param webRequest
+     * @param binderFactory
+     * @return 전달 값
+     */
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/study/config/web/WebConfig.java
+++ src/main/java/study/config/web/WebConfig.java
@@ -1,15 +1,28 @@
 package study.config.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.config.auth.LoginUserArgumentResolver;
 import study.config.interceptor.LoginInterceptor;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Autowired
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+
+    //    @Autowired
 //    LoginInterceptor loginInterceptor;
 //
 //    @Override

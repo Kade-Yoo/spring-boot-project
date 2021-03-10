9a098757264c13f42fec8905a91d6fd46d2a6597
honesty-yoo <yhm9207@naver.com>
2021-03-11 03:57:04 +0900

    IndelController mockMvc로 변경
    - 테스트 코드 수정

--- src/test/java/study/web/IndexControllerTest.java
+++ src/test/java/study/web/IndexControllerTest.java
@@ -1,14 +1,22 @@
 package study.web;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -17,12 +25,26 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void init() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
     @Test
-    public void 메인페이지_로딩() {
+    public void 메인페이지_로딩() throws Exception {
         //when
-        String body = this.restTemplate.getForObject("/mustache", String.class);
+        mvc.perform(get("/mustache"))
+                .andExpect(status().isOk());
+//        String body = this.restTemplate.getForObject("/mustache", String.class);
 
         //then
-        assertThat(body).contains("Naver");
+//        assertThat(body).contains("Naver");
     }
 }

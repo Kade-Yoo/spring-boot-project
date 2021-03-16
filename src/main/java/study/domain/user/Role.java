5c4ed41573aaaa666bbab0a45022d7c9f8e3980d
honesty-yoo <yhm9207@naver.com>
2021-03-17 00:58:59 +0900

    Role name change
    - change role name

--- src/main/java/study/domain/user/Role.java
+++ src/main/java/study/domain/user/Role.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Role {
     GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "일반 사용자");
+    USER("ROLE_USERS", "일반 사용자");
 
     Role(String key, String title) {
         this.key = key;

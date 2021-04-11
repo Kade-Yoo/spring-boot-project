69df117f5ee19606be6f5514f09858b5cede79fc
honesty-yoo <yhm9207@naver.com>
2021-04-12 02:29:23 +0900

    Chapter10 배포스크립트 작성완료
    - Chapter10 배포스크립트 작성완료

---
+++ scripts/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source $(ABSDIR)/profile.sh
+
+REPOSITORY=/home/ec2-user/app/step3
+
+PROJECT_NAME=spring-boot-project
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR_NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profils.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &

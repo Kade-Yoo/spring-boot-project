334fbb482c51ed8abbc913289fd9d1003e8ffa04
honesty-yoo <yhm9207@naver.com>
2021-04-17 23:13:21 +0900

    Chapter10 properties 명칭에 변수사용 방법 변경
    properties 변수 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,5 +25,6 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $IDLE_PROFILE 실행"
+echo "> application-$IDLE_PROFILE 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profils.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profils.active="$IDLE_PROFILE" "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &

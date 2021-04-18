d5c2399cd1907eaf4f300da5b59d7450dde52cd3
honesty-yoo <yhm9207@naver.com>
2021-04-18 09:38:14 +0900

    Chapter10 profile 별 로그 적재파일 변경
    - nohup 경로 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,4 +27,4 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $IDLE_PROFILE 실행"
 echo "> application-$IDLE_PROFILE 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profils.active="$IDLE_PROFILE" "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profils.active="$IDLE_PROFILE" "$JAR_NAME" > $REPOSITORY/nohup-$IDLE_PROFILE.out 2>&1 &
